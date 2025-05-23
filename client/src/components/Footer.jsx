cf2bea81bb6d1dfdd9bbfbc49c0401d9b9fe548f
aravind8204 <aravindhanvgc@gmail.com>
2025-05-23 12:45:03 +0530

    tailwindcss config

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <footer className='bg-gray-800 text-white py-4'>
+            <div className='container mx-auto text-center'>
+                <p> &copy; 2023 Chat App. All rights reserved.</p>
+                <p>Built with ❤️ using React and Vite</p>
+            </div>
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+
+  return (
+    <div id="head">
+        <h1 className=' text-4xl bg-red-700'>Welcome to the Chat App</h1>
+        <p className='text-center'>This is a simple chat application built with React and Vite.</p>
+        <p className='text-center'>Click on the links above to navigate through the app.</p>
+    </div>
+  )
+}
+
+export default Header
